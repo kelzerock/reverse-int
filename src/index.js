b641a5ffe2e972477a8d3d8b0812643f2f4527c4
kelzerock <kelzzzer@gmail.com>
2021-02-28 00:29:53 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  n = Math.abs(n).toString()
+console.log(n)
+  for (let i=0; i<n.length;i++) {
+    result += n[n.length-1-i]
+  }
+  return result
 }
+
